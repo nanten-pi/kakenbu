819f9db01eac7fc7641ac266b495ad8f20efc299
nanten-pi <nantenpizza@gmail.com>
2022-04-09 22:06:23 +0900

    keyboard press find

--- robotmotor/main.py
+++ robotmotor/main.py
@@ -1,4 +1,26 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
 import time
 import sys
-GPIO.setmode(GPIO.BCM)
+from pynput.keyboard import Key, Listener
+GPIO.setmode(GPIO.BCM)#GPiomode is BCM
+GPIO.setup(4, GPIO.OUT) #Amotorpin out (bcm)
+GPIO.setup(17, GPIO.OUT) #Amotorpin out (bcm)
+GPIO.setup(9, GPIO.OUT) #Bmotorpin out (bcm)
+GPIO.setup(11, GPIO.OUT) #Bmotorpin out (bcm)
+
+def on_press(key):#keyboard find?
+    print('{0} pressed'.format(
+        key))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
