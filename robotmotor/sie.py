c10755e61a2b411326e0c6c768c46d4d8f9eff43
nanten-pi <nantenpizza@gmail.com>
2022-04-30 22:57:10 +0900

    hei

---
+++ robotmotor/sie.py
@@ -0,0 +1,33 @@
+import wiringpi
+import time
+import sys
+
+motor_pin_1 = 23
+motor_pin_2 = 24
+
+wiringpi.wiringPiSetupGpio() #initilize GPIO control
+
+print("stop 5sec")
+wiringpi.pinMode( motor_pin_1, 0 )
+wiringpi.pinMode( motor_pin_2, 0 )
+
+print("right 5sec")
+wiringpi.digitalWrite( motor_pin_1, 1 )
+wiringpi.digitalWrite( motor_pin_2, 0 )
+time.sleep(5)
+
+print("stop 5sec")
+wiringpi.digitalWrite( motor_pin_1, 1 )
+wiringpi.digitalWrite( motor_pin_2, 1 )
+time.sleep(5)
+
+print("left 5sec")
+wiringpi.digitalWrite( motor_pin_1, 0 )
+wiringpi.digitalWrite( motor_pin_2, 1 )
+time.sleep(5)
+
+print("stop 5sec")
+wiringpi.digitalWrite( motor_pin_1, 0 )
+wiringpi.digitalWrite( motor_pin_2, 0 )
+
+print("Done")
