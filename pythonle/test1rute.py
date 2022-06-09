d16ed3f1bd2898f6a6a551fa84306c1ac5427ef8
nanten-pi <nantenpizza@gmail.com>
2022-06-09 21:31:51 +0900

    文化祭準備一応

---
+++ pythonle/test1rute.py
@@ -0,0 +1,24 @@
+#␣x␣の 平方 根 を求 め る
+x = 80.2
+#
+rnew = x
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1+r2)/2
+print(r1,rnew,r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1+r2)/2
+print(r1, rnew, r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
